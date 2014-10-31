13b97fdabde39efeccaff1eaccb8c5af1b978c0b
Sandeep Bhardwaj <sandeep.bhardwaj2307@gmail.com>
2014-11-01 01:13:21 +0530

    [Sandeep] Using mocha instead of jasmine.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -3,21 +3,21 @@
 // Generated on 2014-10-31 using
 // generator-karma 0.8.3
 
-module.exports = function(config) {
-  'use strict';
+module.exports = function (config) {
+    'use strict';
 
-  config.set({
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    config.set({
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '../',
+        // base path, that will be used to resolve files and exclude
+        basePath: '../',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['mocha', 'chai'],
 
-    // list of files / patterns to load in the browser
-    files: [
+        // list of files / patterns to load in the browser
+        files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-animate/angular-animate.js',
@@ -31,45 +31,46 @@ module.exports = function(config) {
       'test/spec/**/*.js'
     ],
 
-    // list of files / patterns to exclude
-    exclude: [],
+        // list of files / patterns to exclude
+        exclude: [],
 
-    // web server port
-    port: 8080,
+        // web server port
+        port: 8080,
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: [
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: [
       'PhantomJS'
     ],
 
-    // Which plugins to enable
-    plugins: [
+        // Which plugins to enable
+        plugins: [
       'karma-phantomjs-launcher',
-      'karma-jasmine'
+      'karma-mocha',
+      'karma-chai'
     ],
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false,
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false,
 
-    colors: true,
+        colors: true,
 
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    // Uncomment the following lines if you are using grunt's server to run the tests
-    // proxies: {
-    //   '/': 'http://localhost:9000/'
-    // },
-    // URL root prevent conflicts with the site root
-    // urlRoot: '_karma_'
-  });
+        // Uncomment the following lines if you are using grunt's server to run the tests
+        // proxies: {
+        //   '/': 'http://localhost:9000/'
+        // },
+        // URL root prevent conflicts with the site root
+        // urlRoot: '_karma_'
+    });
 };
