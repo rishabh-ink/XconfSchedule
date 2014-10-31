adfc5299c334620b2f06768c840d0f14cda14025
Sandeep Bhardwaj <sandeep.bhardwaj2307@gmail.com>
2014-11-01 00:52:04 +0530

    [Sandeep] Using yeoman to generate app for angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xconfScheduleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the xconfScheduleApp
+ */
+angular.module('xconfScheduleApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xconfScheduleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xconfScheduleApp
+ */
+angular.module('xconfScheduleApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
