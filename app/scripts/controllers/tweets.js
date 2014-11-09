84e70e2aa3973f446e682dd7c2f97215243580ab
Sandeep Bhardwaj <sandeep.bhardwaj2307@gmail.com>
2014-11-09 22:25:23 +0530

    [Sandeep] Trying to fetch tweets

--- app/scripts/controllers/tweets.js
+++ app/scripts/controllers/tweets.js
@@ -1,7 +1,18 @@
 'use strict';
 
+/**
+ * @ngdoc function
+ * @name xconfScheduleApp.controller:TwitterController
+ * @description
+ * # TwitterController
+ * Controller of the xconfScheduleApp
+ */
 angular.module('xconfScheduleApp')
-    .controller('TwitterController', ['$scope',
-        function ($scope) {
-            $scope.hashtag = 'football';
-    }]);
+    .controller('TwitterController', ['$scope', '$http',
+    function ($scope, $http) {
+
+        $http.get('data.json').success(function (data) {
+            $scope.tweets = data;
+        });
+    }
+}]);
